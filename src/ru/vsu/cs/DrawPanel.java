d03c58ac741be791e59d86f6ad925360da32f892
Annaseek <agapova.nyura@bk.ru>
2020-09-11 14:58:21 +0300

    Добавление муравьев, распределение их в случайном порядке по полю

--- src/ru/vsu/cs/DrawPanel.java
+++ src/ru/vsu/cs/DrawPanel.java
@@ -14,7 +14,7 @@ public class DrawPanel extends JPanel {
     public void drawPicture(Graphics2D gr) {
         Random random = new Random();
 
-        int numberOfAnts = 300;
+
         int horizon = getHeight() / 2 - 50;
         int xSun = 400;
         int ySun = 100;
@@ -26,6 +26,9 @@ public class DrawPanel extends JPanel {
         int heightOfClouds = 60;
         int[] xAnthill = {getWidth() - 300, getWidth() - 450, getWidth() - 50, getWidth() - 200};
         int[] yAnthill = {horizon - 2 * heightOfClouds, horizon + 100, horizon + 100, horizon - 2 * heightOfClouds};
+        int numberOfAnts = 45;
+        int weightOfAnts = 30;
+        int heightOfAnts = 25;
         int[] xAnteaterNose = {0, 0, 100, 110};
         int[] yAnteaterNose = {horizon - 120, horizon - 35, horizon - 10, horizon - 20};
 
@@ -37,19 +40,19 @@ public class DrawPanel extends JPanel {
                     random.nextInt(horizon - 2 * heightOfClouds), weightOfClouds, heightOfClouds);
         }
         drawAnthill(gr, xAnthill, yAnthill);
+
         for (int i = 0; i < numberOfAnts; i++) {
-//            drawAnt(gr);
+            drawAnt(gr, random.nextInt(getWidth()),
+                    horizon + random.nextInt(getHeight() - horizon - heightOfAnts), weightOfAnts, heightOfAnts);
         }
         drawAnteaterNose(gr, xAnteaterNose, yAnteaterNose);
     }
 
-    private void drawAnt(Graphics2D g, int x, int y) {
-
-    }
-
-    private void drawAnthill(Graphics2D g, int[] x, int[] y) {
-        g.setColor(new Color(102, 0, 51));
-        g.fillPolygon(x, y, 4);
+    private void drawBackground(Graphics2D g, int y) {          //y-координата, где "сходятся" земля и небо
+        g.setColor(new Color(204, 255, 255));
+        g.fillRect(0, 0, getWidth(), y);
+        g.setColor(new Color(51, 153, 33));
+        g.fillRect(0, y, getWidth(), getHeight() - y + 1);
     }
 
     private void drawSun(Graphics2D g, int x, int y, int r1, int r2, int n) {
@@ -76,16 +79,26 @@ public class DrawPanel extends JPanel {
         g.fillOval(x, y, w, h);
     }
 
+    private void drawAnthill(Graphics2D g, int[] x, int[] y) {
+        g.setColor(new Color(102, 0, 51));
+        g.fillPolygon(x, y, 4);
+    }
+
+    private void drawAnt(Graphics2D g, int x, int y, int weight, int height) {
+        g.setColor(new Color(51, 0, 0));
+        int w = weight / 3;
+        int h = height / 3;
+        g.fillOval(x, y + h, 2 * w, 2 * h);
+        g.fillOval(x + w, y, 2 * w, 2 * h);
+        g.drawLine(x + w, y + height - h, x + w, y + height + h / 2);
+        g.drawLine(x + w, y + height - h, x + 2 * w, y + height + h / 2);
+        g.drawLine(x + w, y + height - h, x, y + height + h / 2);
+    }
+
     private void drawAnteaterNose(Graphics2D g, int[] x, int[] y) {
         g.setColor(new Color(153, 102, 102));
         g.fillPolygon(x, y, 4);
     }
 
-    private void drawBackground(Graphics2D g, int y) {          //y-координата, где "сходятся" земля и небо
-        g.setColor(new Color(204, 255, 255));
-        g.fillRect(0, 0, getWidth(), y);
-        g.setColor(new Color(51, 153, 33));
-        g.fillRect(0, y, getWidth(), getHeight() - y + 1);
-    }
 
 }
